a92201cc17823c50f56562e448891bbdf3614cc6
Fireskorpion123 <fireskorpion123@gmail.com>
2025-08-05 11:39:51 -0400

    finished

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Your code goes here
+let nameForm = document.getElementById('name-form')
+let userInput = document.getElementById('user-input')
+let computerResponse = document.getElementById("computer-response")
+let enemy = ["Timmy", "George", "Tamamo", "Tom"]
+
+nameForm.addEventListener("submit", function (event) {
+    event.preventDefault()
+    console.log(userInput.value)
+    let userName = userInput.value.trim()
+    if (enemy.includes(userName)) {
+        computerResponse.textContent = "Go away."
+    } else {
+        computerResponse.textContent = `Hello, ${userName}.`
+    }
+})
